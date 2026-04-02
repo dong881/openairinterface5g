c598fcf7524fd919b6c9f5e827a384c3bc2fcee6
francescomani <email@francescomani.it>
2026-04-02 12:27:03 +0200

    functions to decode UCI on PUSCH

--- openair1/PHY/CODING/nrSmallBlock/nr_small_block_defs.h
+++ openair1/PHY/CODING/nrSmallBlock/nr_small_block_defs.h
@@ -15,8 +15,9 @@
 
 
 uint32_t encodeSmallBlock(int in, int len);
-
 uint16_t decodeSmallBlock(int8_t *in, uint8_t len);
+void nr_rate_matching_smallcodeblock_rx(int16_t *input, int E, int N, int8_t *output);
+uint16_t decode_1_2_uci_bit(int8_t *input, int n_bits, int qam);
 
 static const uint32_t nrSmallBlockBasis[11] = {0xFFFFFFFF, 0x4BA5A933, 0x7D910E5A, 0x6D26339C, 0x71C7C3E0,
 		0x7E0FFC00, 0x731D8E64, 0x6B44F5B0, 0x7DC218EC, 0x4DA1B746, 0x42F0FFFF};
